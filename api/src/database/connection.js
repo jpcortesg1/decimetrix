9fd3a3c99720a13ef35b200d07ce123bb8322862
jpcortesg1 <jpcortesg@eafit.edu.co>
2022-04-08 13:38:20 -0500

    create connection to heroku with serve

--- api/src/database/connection.js
+++ api/src/database/connection.js
@@ -1,6 +1,25 @@
 import config from "../config";
 import { Sequelize, DataTypes, Model } from "sequelize";
 
-const sequelize = new Sequelize(config.databaseUrl);
+// Build
+const sequelize = new Sequelize(config.databaseUrl, {
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+});
+
+// Dev
+// const sequelize = new Sequelize(
+//   config.nameDb,
+//   config.userDb,
+//   config.passwordDb,
+//   {
+//     host: config.hostDb,
+//     dialect: "postgres",
+//   }
+// );
 
 export { sequelize, DataTypes, Sequelize, Model };
